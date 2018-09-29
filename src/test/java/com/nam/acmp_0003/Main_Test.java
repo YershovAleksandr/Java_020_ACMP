088f650401d943feac56d049b06386ad962c686c
hp <namstudionsk@gmail.com>
2018-09-29 20:50:01 +0700

    Add 0003 task

---
+++ src/test/java/com/nam/acmp_0003/Main_Test.java
@@ -0,0 +1,18 @@
+package com.nam.acmp_0003;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Main_Test {
+
+    @Test
+    public void shouldAnswerWithTrue(){
+        Assert.assertEquals(5 * 5, Main.MyFunc(5));
+        Assert.assertEquals(45 * 45, Main.MyFunc(45));
+        Assert.assertEquals(35 * 35, Main.MyFunc(35));
+        Assert.assertEquals(95 * 95, Main.MyFunc(95));
+        Assert.assertEquals(125 * 125, Main.MyFunc(125));
+        Assert.assertEquals(75 * 75, Main.MyFunc(75));
+        Assert.assertEquals(4255 * 4255, Main.MyFunc(4255));
+    }
+}
