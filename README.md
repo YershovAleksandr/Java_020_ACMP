f1f941b9eba96b002d0dd680faf222e2e91d364a
hp <namstudionsk@gmail.com>
2018-10-08 16:31:51 +0700

    Add Task 0061

--- README.md
+++ README.md
@@ -19,6 +19,8 @@ Java console applications for acmp.ru
 
 0033
 
+0061
+
 0108
 
 0195
---
+++ src/main/java/com/nam/acmp_0061/Main.java
@@ -0,0 +1,41 @@
+package com.nam.acmp_0061;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        PrintWriter out = new PrintWriter(System.out);
+
+        int[] resA = new int[4];
+        int[] resB = new int[4];
+
+        int aWin = 0;
+        int bWin = 0;
+
+        for (int i = 0; i < 4; i++){
+            resA[i] = in.nextInt();
+            resB[i] = in.nextInt();
+
+            aWin += resA[i];
+            bWin += resB[i];
+            /*if (resA[i] > resB[i]){
+                aWin++;
+            } else if (resA[i] < resB[i]){
+                bWin++;
+            }*/
+        }
+
+        if (aWin > bWin){
+            out.println("1");
+        } else if (bWin > aWin){
+            out.println("2");
+        } else {
+            out.println("DRAW");
+        }
+
+        out.flush();
+    }
+}
