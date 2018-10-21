36c2f7681e136d6815653f37fbd1b7e157a6f5b5
hp <namstudionsk@gmail.com>
2018-10-21 23:20:03 +0700

    Add Task 0066 lv

---
+++ src/main/java/com/nam/acmp_0066/Main.java
@@ -0,0 +1,21 @@
+package com.nam.acmp_0066;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        String keyboardChars = "qwertyuiopasdfghjklzxcvbnm";
+
+        char c = in.next().charAt(0);
+
+        for (int i = 0; i < keyboardChars.length(); i++){
+            //
+        }
+
+        out.flush();
+    }
+}
