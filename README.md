5fce2168defae22da67ad8f245d45ca6138f5611
hp <namstudionsk@gmail.com>
2018-10-22 23:39:50 +0700

    Add Task 0023

--- README.md
+++ README.md
@@ -15,6 +15,8 @@ Java console applications for acmp.ru
 
 0021
 
+0023
+
 0025
 
 0033
---
+++ src/main/java/com/nam/acmp_0023/Main.java
@@ -0,0 +1,25 @@
+package com.nam.acmp_0023;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+
+        int sum = 0;
+
+        for (int i = 2; i < n; i++){
+            if (n % i == 0){
+                sum += i;
+            }
+        }
+
+        out.println(sum + 1 + n);
+
+        out.flush();
+    }
+}
