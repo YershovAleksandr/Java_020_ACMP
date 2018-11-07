06f9fc30171454d4b355d8dbe8604b245cf53474
hp <namstudionsk@gmail.com>
2018-11-08 01:42:24 +0700

    Add Task 0943

---
+++ src/main/java/com/nam/acmp_0943.java
@@ -0,0 +1,24 @@
+package com.nam;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class acmp_0943 {
+    public static void main(String[] args){
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int y = in.nextInt();
+        int x = in.nextInt();
+
+        if (y % 2 != 0){
+            out.println(m * (y - 1) + x - 1);
+        } else {
+            out.println(m * y  - x);
+        }
+
+        out.flush();
+    }
+}
