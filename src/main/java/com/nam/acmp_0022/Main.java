feb07bf8f1c831feab1c9d83de10e5542eaad734
hp <namstudionsk@gmail.com>
2018-11-08 01:11:19 +0700

    Add Task 0022

---
+++ src/main/java/com/nam/acmp_0022/Main.java
@@ -0,0 +1,26 @@
+package com.nam.acmp_0022;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        int count = 0;
+
+        while (n  > 0){
+            if (n % 2 != 0){
+                count++;
+            }
+            n /= 2;
+        }
+
+        out.println(count);
+
+        out.flush();
+    }
+}
