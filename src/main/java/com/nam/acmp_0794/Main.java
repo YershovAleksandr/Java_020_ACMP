32652738766c49dcdc8ff3c66c8f3c2ce461e036
hp <namstudionsk@gmail.com>
2018-11-08 00:15:15 +0700

    Add Task 0794

--- src/main/java/com/nam/acmp_0794/Main.java
+++ src/main/java/com/nam/acmp_0794/Main.java
@@ -8,6 +8,12 @@ public class Main {
         PrintWriter out = new PrintWriter(System.out);
         Scanner in = new Scanner(System.in);
 
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int k = in.nextInt();
+
+        out.println(n * (m < k?m :(m / k + (k - 1))));
+
         out.flush();
     }
 }
