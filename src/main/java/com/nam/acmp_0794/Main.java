fcfb26e62eae1a304b5593485de3ab5aaa6ecdef
hp <namstudionsk@gmail.com>
2018-11-07 23:59:10 +0700

    Add Task 0794 lv

--- src/main/java/com/nam/acmp_0794/Main.java
+++ src/main/java/com/nam/acmp_0794/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args){
         PrintWriter out = new PrintWriter(System.out);
 
+
         Scanner in = new Scanner(System.in);
 
         out.flush();
