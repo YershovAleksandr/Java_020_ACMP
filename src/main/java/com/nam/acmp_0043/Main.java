2a6b5c75034a5411f9e6ba61930746e599e99927
hp <namstudionsk@gmail.com>
2019-01-22 17:45:08 +0700

    Add Task 0043

---
+++ src/main/java/com/nam/acmp_0043/Main.java
@@ -0,0 +1,31 @@
+package com.nam.acmp_0043;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        String str = in.next();
+
+        int length = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == '0'){
+                length++;
+            } else{
+                length = 0;
+            }
+            if (maxLength < length){
+                maxLength = length;
+            }
+        }
+
+        out.println(maxLength);
+
+        out.flush();
+    }
+}
