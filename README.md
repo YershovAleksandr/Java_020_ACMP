0349206072fcb289fb46cc50c3f7a83412e04db0
hp <namstudionsk@gmail.com>
2019-02-17 01:01:17 +0700

    Added task 0952

--- README.md
+++ README.md
@@ -146,4 +146,6 @@ Java console applications for acmp.ru
 0948
 0949
 
+0952
+
 0970
--- src/main/java/com/nam/_900/acmp_0952/Main.java
+++ src/main/java/com/nam/_900/acmp_0952/Main.java
@@ -8,6 +8,31 @@ public class Main {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
 
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        int min;// = n + (m - n);
+        int max;
+
+        if (n >= m){
+            min = n;
+        }else{
+            min = n + m - n;
+        }
+
+        if (m > 0){
+            max = n + m - 1;
+        }else{
+            max = n;
+        }
+
+        if (n == 0 && m > 0) {
+            out.println("Impossible");
+        }else if (n == 0 && m == 0){
+            out.println("0 0");
+        }else{
+            out.println(String.format("%d %d", min, max));
+        }
 
         out.flush();
     }
